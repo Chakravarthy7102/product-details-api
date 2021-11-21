3fa9a6fb09c50a3000a27abbbd4c02911715655f
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2021-11-21 17:08:43 +0530

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -3,14 +3,15 @@ const ProductsModel = require("../models/ProductsModel");
 //controllers
 //testing route
 const getAllProductsStatic = async (req, res) => {
-  const body = await ProductsModel.find().sort("-name");
-  res.status(200).json({ noResult: body.length, body: { body } });
+  const { select } = req.query;
+  const result = await ProductsModel.find().select(select).limit(5);
+  res.status(200).json({ noResult: result.length, result: { result } });
 };
 
 const getAllProducts = async (req, res) => {
   //making sure that the query params are only ones that are present in database not
   //any random params..
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   //creating new params object that will pass in the valid params into the find
   const queryObject = {};
   //checking for the value of featured and setting it into the query object JMS
@@ -31,18 +32,35 @@ const getAllProducts = async (req, res) => {
 
   console.log(queryObject);
   //removing the await because of addition chainning of the sort params
-  let body = ProductsModel.find(queryObject);
+  let result = ProductsModel.find(queryObject);
+
+  // //limit results
+  // if (limit) {
+  //   result = result.limit(limit * 1); //by multiplying a string with number any number it becomes a number
+  // }
+
   //implement the sorting of the data
   //line 51
   if (sort) {
     const sortList = sort.split(",").join(" ");
-    body = await body.sort(sortList);
-    console.log(sortList);
+    result = result.sort(sortList);
   } else {
-    body = await body.sort("createdAt");
+    result = result.sort("createdAt");
   }
-  const Products = await body;
-  res.status(200).json({ Products, noHits: body.length });
+  //select only some fileds
+  if (fields) {
+    const fieldList = fields.split(",").join(" ").toString();
+    result = result.select(fieldList);
+  }
+  //pagination feature
+  const limit = req.query.limit * 1 || 10;
+  const page = req.query.page * 1 || 1;
+  const skipIndex = (page - 1) * limit;
+
+  result = result.limit(limit).skip(skipIndex);
+
+  const Products = await result; //use this await at the end of giving out the final result
+  res.status(200).json({ noHits: Products.length, Products });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
@@ -53,3 +71,6 @@ module.exports = { getAllProducts, getAllProductsStatic };
 //resons for removing the await form the first fetch of data and using let keyword
 //let helps in modifying the data inside the first stop data and
 //reoving of await removes the error confilcts
+
+//for more deep and clean understafing of the sort and functinalities
+//https://mongoosejs.com/docs/queries.html
